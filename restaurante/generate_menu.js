42d26ae282dc92fdd57526e31a8b4969d9710341
lamascr <lamascpo@gmail.com>
2026-01-17 14:47:42 +0100

    2

--- restaurante/generate_menu.js
+++ restaurante/generate_menu.js
@@ -1,31 +1,11 @@
 const XLSX = require('xlsx');
 
 const data = [
-    {
-        category: 'entrantes',
-        name: 'Carpaccio de Wagyu',
-        price: 28,
-        description: 'Láminas de Wagyu A5, trufa negra y lascas de Parmigiano Reggiano 36 meses.',
-        image: 'https://images.unsplash.com/photo-1546241072480-10ad28c2c?auto=format&fit=crop&q=80&w=800' // Fixed URL slightly, removed random char if any, keeping original mostly
-    },
-    {
-        category: 'principales',
-        name: 'Lubina Salvaje',
-        price: 42,
-        description: 'Costra de sal, emulsión de algas y verduras de temporada glaceadas.',
-        image: 'https://images.unsplash.com/photo-1559339352-11d035aa65de?auto=format&fit=crop&q=80&w=800'
-    },
-    {
-        category: 'postres',
-        name: 'Texturas de Chocolate',
-        price: 16,
-        description: 'Valrhona 70%, frutos rojos y helado artesanal de vainilla Bourbon.',
-        image: 'https://images.unsplash.com/photo-1551024506-0bccd828d307?auto=format&fit=crop&q=80&w=800'
-    }
+
 ];
 
 const ws = XLSX.utils.json_to_sheet(data);
 const wb = XLSX.utils.book_new();
 XLSX.utils.book_append_sheet(wb, ws, "Menu");
-XLSX.writeFile(wb, "menu.xlsx");
+XLSX.writeFile(wb, "https://docs.google.com/spreadsheets/d/13B57Xr7FTrUdZ8nuy8ex4zWFv0F8Ve3zhpLXlt6-H5c/edit?usp=drive_link");
 console.log("menu.xlsx created successfully!");
